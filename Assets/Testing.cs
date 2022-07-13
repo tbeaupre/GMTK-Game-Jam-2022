38f59834dd3616861da896ae091938291c005f00
Finn Westergren <fwesterg5@gmail.com>
2022-07-13 17:09:53 -0400

    messin around with SFX
    birds

--- Assets/Testing.cs
+++ Assets/Testing.cs
@@ -2,17 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class Testing : MonoBehaviour
 {
+    private AudioSource _audioSource;
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Testing to see if Finn can get this code working!");
+        _audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            _audioSource.Play();
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            _audioSource.Stop();
+        }
     }
 }
