9480ef1fbe8c36c372af0850d19ef2c9687fa981
Tyler Beaupre <tbeaupre@wpi.edu>
2022-07-13 16:43:40 -0400

    Testing for Finn

---
+++ Assets/Testing.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testing : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Testing to see if Finn can get this code working!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
