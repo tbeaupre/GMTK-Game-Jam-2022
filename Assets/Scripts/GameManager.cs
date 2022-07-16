547c6a4dd14d06ed446a0a960ec546dfeef507f7
Finn Westergren <fwesterg5@gmail.com>
2022-07-16 18:58:32 -0400

    map edits

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -75,7 +75,7 @@ public class GameManager : MonoBehaviour
             Init();
         }
 
-        if (isDebugMode) return; // not sure why this would happen but just in case
+        if (isDebugMode || activeMap == null) return; // not sure why this would happen but just in case
 
         if (goalTileToDelete != null && !player.tile.PositionalMatch(goalTileToDelete)) //player has recently left a goal tile
         {
